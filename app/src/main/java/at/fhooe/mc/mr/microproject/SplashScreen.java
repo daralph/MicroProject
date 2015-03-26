d8ed8b3b562c88b3f936bf45e9e638e5e4238442
YourDirtyMum <who-i-am@gmx.at>
2015-03-26 09:29:36 +0100

    SplashScreen configuration

--- app/src/main/java/at/fhooe/mc/mr/microproject/SplashScreen.java
+++ app/src/main/java/at/fhooe/mc/mr/microproject/SplashScreen.java
@@ -28,7 +28,7 @@ public class SplashScreen extends Activity {
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    private static final int AUTO_HIDE_DELAY_MILLIS = 1000;
 
     /**
      * If set, will toggle the system UI visibility upon interaction. Otherwise,
@@ -108,12 +108,6 @@ public class SplashScreen extends Activity {
                 }
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        // this is a test comment
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
